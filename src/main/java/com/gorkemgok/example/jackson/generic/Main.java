f045a401e6374e061050ee10614b993e0b288dd0
Gorkem Gok <gorkem.gok@proj-e.com>
2019-08-15 23:25:47 +0300

    Initial commit

--- src/main/java/com/gorkemgok/example/jackson/generic/Main.java
+++ src/main/java/com/gorkemgok/example/jackson/generic/Main.java
@@ -1,6 +1,5 @@
 package com.gorkemgok.example.jackson.generic;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
